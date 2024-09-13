98c00089fed8d0f2f16bc6f111364195a5638be7
Hubab777 <159882654+Hubab777@users.noreply.github.com>
2024-09-13 19:10:55 +0500

    Add files via upload

---
+++ Milestone 1 & 2/script.ts
@@ -0,0 +1,12 @@
+
+const toggledButton = document.getElementById('toggle-skills') as HTMLButtonElement;
+
+const Skills = document.getElementById('skills') as HTMLElement;
+
+toggledButton.addEventListener('click', ()=> {
+    if(skills.style.display === 'none') {
+        skills.style.display = 'block'
+    } else {
+        skills.style.display = 'none'
+    }
+});
